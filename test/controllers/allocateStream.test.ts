6d5aaf414b5cc15d1e5c67fc7218c359e35c8ab8
miguskar@gmail.com <miguskar@gmail.com>
2018-05-20 14:57:14 +0200

    test(allocateStream.controller): Add tests for api

--- test/controllers/allocateStream.test.ts
+++ test/controllers/allocateStream.test.ts
@@ -1,13 +1,63 @@
-import request from 'supertest';
+import request, { SuperTest, Test } from 'supertest';
+
 import app from '../../src/app';
+import LoadBalancer from '../../src/logic/loadBalancer';
+jest.mock('../../src/logic/loadBalancer');
 
 describe('Allocate Stream API', () => {
     describe('POST', () => {
-        test('should return 501 not implemented', () => {
-            return request(app).get('/allocateStream').then(res => {
-                expect(res.status).toEqual(501);
-                expect(res.body.message).toEqual('Not Implemented');
+        let req: SuperTest<Test>;
+        const mock = (LoadBalancer as jest.Mock<LoadBalancer>);
+
+        beforeEach(() => {
+            req = request(app);
+            mock.mockClear();
+        });
+
+        test('should respond with 400 if channelId is not set', () => {
+            return req.post('/allocateStream')
+                .set('Accept', 'application/json')
+                .then(res => {
+                    expect(res.status).toEqual(400);
+                    expect(res.body.message).toEqual('\'channelId\' is required');
+                });
+        });
+
+        test('should respond with 200 and url', () => {
+            const fakeURL = 'http://fake-uri.example.com';
+            // mock load balancer to return a url
+            mock.mockImplementation(() => {
+                return {
+                    getAvailableServerURL: () => {
+                        return Promise.resolve(fakeURL);
+                    },
+                };
             });
+            return req.post('/allocateStream')
+                .send({ channelId: 'svt1' })
+                .set('Accept', 'application/json')
+                .then(res => {
+                    expect(res.status).toEqual(200);
+                    expect(res.body.url).toEqual(fakeURL);
+                });
+        });
+
+        test('should respond with 500 and error', () => {
+            const fakeError = new Error('No available servers');
+            // mock load balancer to return a url
+            mock.mockImplementation(() => {
+                return {
+                    getAvailableServerURL: () => {
+                        return Promise.reject(fakeError);
+                    },
+                };
+            });
+            return req.post('/allocateStream')
+                .send({ channelId: 'svt1' })
+                .set('Accept', 'application/json')
+                .then(res => {
+                    expect(res.status).toEqual(500);
+                });
         });
     });
 });
