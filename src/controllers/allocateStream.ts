8d15ba6bcd08e30d92b13d69713ab68f42397887
miguskar@gmail.com <miguskar@gmail.com>
2018-05-20 20:20:44 +0200

    feat(controllers:allocateStream): implement controller

--- src/controllers/allocateStream.ts
+++ src/controllers/allocateStream.ts
@@ -1,5 +1,16 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator/check';
 
-export function allocate(req: Request, res: Response) {
-  res.status(501).json({ message: 'Not Implemented' });
+import LoadBalancer from '../logic/loadBalancer';
+import { HttpError } from '../utils/errors';
+
+export async function allocate(req: Request, res: Response, next: NextFunction) {
+  const lb = new LoadBalancer();
+  const body: { channelId: string } = req.body;
+  try {
+    const url = await lb.getAvailableServerURL(body);
+    res.json({ url });
+  } catch (err) {
+    next(new HttpError(err.message, 500));
+  }
 }
