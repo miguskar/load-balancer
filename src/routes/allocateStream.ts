87976adc253bcf57ddaaeaf3f5efacb8b3446672
miguskar@gmail.com <miguskar@gmail.com>
2018-05-20 20:19:37 +0200

    feat(routes:allocateStream): validate body

--- src/routes/allocateStream.ts
+++ src/routes/allocateStream.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import { body } from 'express-validator/check';
 import { allocate } from '../controllers/allocateStream';
+import { validator } from '../middlewares/validator';
 
 const router = Router();
 
-router.post('/', allocate);
+router.post('/', [body('channelId').exists()], validator, allocate);
 
 export default router;
