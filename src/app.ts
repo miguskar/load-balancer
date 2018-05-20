d2dc8260e2a52090f8573760e945a6b5709ac74e
miguskar@gmail.com <miguskar@gmail.com>
2018-05-20 15:58:55 +0200

    feat(app): use json body parser. use express-validator

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import expressValidator from 'express-validator';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+app.use(expressValidator());
+
+// add routes
 app.use(routes);
 
 export default app;
