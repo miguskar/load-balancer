20cd9702d4b1661a2cdd386dd4ea7d5b338077a8
miguskar@gmail.com <miguskar@gmail.com>
2018-05-23 07:59:28 +0200

    wip(loadBalancer):

--- src/logic/loadBalancer.ts
+++ src/logic/loadBalancer.ts
@@ -1,8 +1,44 @@
-export default class LoadBalancer {
-  private SERVER_URIS = ['case1-1.neti.systems', 'case1-2.neti.systems', 'case1-3.neti.systems'];
-  constructor() { }
+import request from 'request-promise';
+import { HttpError } from '../utils/errors';
 
-  async getAvailableServerURL(): Promise<string> {
-    throw Error('Not implemented');
+export default class LoadBalancer {
+  public static SERVER_URIS = [
+    'case1-1.neti.systems',
+    'case1-2.neti.systems',
+    'case1-3.neti.systems'
+  ];
+  private static nextIndex = 0;
+
+  static reset() {
+    LoadBalancer.nextIndex = 0;
+  }
+
+  constructor() {}
+
+  async getAvailableServerURL(body: { channelId: string }): Promise<string> {
+    const startIndex = LoadBalancer.nextIndex;
+    const noOfServers = LoadBalancer.SERVER_URIS.length;
+    const endIndex = startIndex + noOfServers;
+    for (let i = startIndex; i < endIndex; ++i) {
+      const serverUrl = LoadBalancer.SERVER_URIS[i % noOfServers];
+      try {
+        LoadBalancer.nextIndex = (i + 1) % noOfServers;
+        const { url } = await this.sendRequest(`http://${serverUrl}:3000/allocateStream`, body);
+        console.log('setting next index to ', (i + 1) % noOfServers);
+        return url;
+      } catch (err) {
+        console.log('got error', err);
+        // If we ended up here, it means the request either timed out or we got an error
+        // When that happens, we should try the next server
+      }
+    }
+    return Promise.reject(new HttpError('No servers available', 500));
+  }
+
+  private async sendRequest(
+    url: string,
+    body: { channelId: string }
+  ): Promise<{ url: string; secret: string }> {
+    return request(url, { timeout: 1000, method: 'POST', body, json: true });
   }
 }
--- test/logic/loadBalancer.test.ts
+++ test/logic/loadBalancer.test.ts
@@ -1,12 +1,50 @@
 import LoadBalancer from '../../src/logic/loadBalancer';
+import nock from 'nock';
 
 describe('Load balancer', () => {
-    let lb: LoadBalancer;
-    beforeEach(() => {
-        lb = new LoadBalancer();
+  let lb: LoadBalancer = new LoadBalancer();
+
+  test('should be instantiated', () => {
+    expect(lb).toBeDefined();
+  });
+
+  describe('when all servers work', () => {
+    beforeAll(() => {
+      for (const url of LoadBalancer.SERVER_URIS) {
+        nock(`http://${url}:3000`)
+          .post('/allocateStream')
+          .reply(200, { url });
+      }
+      const firstUrl = LoadBalancer.SERVER_URIS[0];
+      nock(`http://${firstUrl}:3000`)
+        .post('/allocateStream')
+        .reply(200, { url: firstUrl });
     });
 
-    test('should be instantiated', () => {
-        expect(lb).toBeDefined();
+    let returnedUrl;
+
+    beforeEach(async () => {
+      console.log('creating new LB');
+      lb = new LoadBalancer();
+      returnedUrl = await lb.getAvailableServerURL({ channelId: 'test' });
     });
+
+    test('it should call the first url', () => {
+      expect(returnedUrl).toEqual(LoadBalancer.SERVER_URIS[0]);
+    });
+
+    test('it should call the second url', () => {
+      expect(returnedUrl).toEqual(LoadBalancer.SERVER_URIS[1]);
+    });
+
+    test('it should call the third url', () => {
+      expect(returnedUrl).toEqual(LoadBalancer.SERVER_URIS[2]);
+    });
+
+    test('it should call the first url again', () => {
+      expect(returnedUrl).toEqual(LoadBalancer.SERVER_URIS[0]);
+    });
+  });
+
+  describe('when first server breaks');
 });
